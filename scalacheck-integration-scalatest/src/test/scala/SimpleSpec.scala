df1f195e48ad33bd9901058a91e5daab5a6c153b
Oscar Renalias <oscar@renalias.net>
2011-08-16 17:00:59 +0300

    Added the same properties but now implemented with the Checkers trait instead of PropertyChecks

--- scalacheck-integration-scalatest/src/test/scala/SimpleSpec.scala
+++ scalacheck-integration-scalatest/src/test/scala/SimpleSpec.scala
@@ -1,9 +1,9 @@
 package com.company.scalacheck
 
 import org.scalatest.PropSpec
-import org.scalatest.prop.PropertyChecks
+import org.scalatest.prop.{PropertyChecks, Checkers}
 import org.scalatest.matchers.ShouldMatchers
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Prop._
 
 /**
  * This is an example spec written in ScalaTest using ScalaCheck property checks.
@@ -41,5 +41,29 @@ class SimplePropertySpec extends PropSpec with PropertyChecks with ShouldMatcher
 				a.charAt(a.length-1) should be(a.reverse.charAt(0))
 			}
 		}
+	}
+}
+
+/**
+ * This is the same specification above but written using the Checkers trait
+ */
+class SimplePropertyCheckersSpec extends PropSpec with Checkers {
+
+	property("String should append each other with the concat method") {
+		check(forAll { (a:String, b:String) =>
+			a.concat(b) == (a + b)
+		})
+	}
+
+	property("This property should fail") {
+		check(forAll { (a: String, b: String) =>
+			a.length + b.length == ((a + b).length + 1) // Should fail
+		})
+	}
+	
+	property("Reverse non-empty strings correctly") {
+		check(forAll { (a:String) =>
+			(a.length > 0) ==> (a.charAt(a.length-1) == (a.reverse.charAt(0)))
+		})
 	}
 }
