63d7e7772a19b8d096d8dab04030fb5686a6527c
Oscar Renalias <oscar@renalias.net>
2011-09-14 02:38:22 +0300

    Changes

--- java-hadoop-scalacheck/runwordcount.sh
+++ java-hadoop-scalacheck/runwordcount.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mvn clean package
+rm -rf output
 hadoop jar target/Java-Hadoop-ScalaCheck-1.0-SNAPSHOT.jar com.company.hadoop.WordCount input output
--- java-hadoop-scalacheck/src/main/java/com/company/hadoop/WordCount.java
+++ java-hadoop-scalacheck/src/main/java/com/company/hadoop/WordCount.java
@@ -1,57 +1,56 @@
 package com.company.hadoop;
 
-import java.io.IOException;
-import java.util.*;
-
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class WordCount {
 
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-      private final static IntWritable one = new IntWritable(1);
-      private Text word = new Text();
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
 
-      public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        String line = value.toString();
-        StringTokenizer tokenizer = new StringTokenizer(line);
-        while (tokenizer.hasMoreTokens()) {
-          word.set(tokenizer.nextToken());
-          output.collect(word, one);
-        }
-      }
-    }
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				context.write(word, one);
+			}
+		}
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-      public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        int sum = 0;
-        while (values.hasNext()) {
-          sum += values.next().get();
-        }
-        output.collect(key, new IntWritable(sum));
-      }
-    }
+	}
 
-    public static void main(String[] args) throws Exception {
-      JobConf conf = new JobConf(WordCount.class);
-      conf.setJobName("wordcount");
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-      conf.setOutputKeyClass(Text.class);
-      conf.setOutputValueClass(IntWritable.class);
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
 
-      conf.setMapperClass(Map.class);
-      conf.setCombinerClass(Reduce.class);
-      conf.setReducerClass(Reduce.class);
+	public static void main(String[] args) throws Exception {
+		Job job = new Job();
 
-      conf.setInputFormat(TextInputFormat.class);
-      conf.setOutputFormat(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 
-      FileInputFormat.setInputPaths(conf, new Path(args[0]));
-      FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-
-      JobClient.runJob(conf);
-    }
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
