407928bb4bdc4626bdc62557bad8e6e08fddbe58
Oscar Renalias <oscar@renalias.net>
2011-09-16 00:29:51 +0300

    Empty keys should be ignored.

--- java-hadoop-scalacheck/src/main/java/com/company/hadoop/WordCount.java
+++ java-hadoop-scalacheck/src/main/java/com/company/hadoop/WordCount.java
@@ -33,6 +33,11 @@ public class WordCount {
 	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			// do nothing for empty keys
+			if(key.getLength() == 0)
+				return;
+
+			// otherwise sump up the values
 			int sum = 0;
 			for (IntWritable value : values) {
 				sum += value.get();
